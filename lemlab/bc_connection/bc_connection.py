836548c555cc7d5f288a4401da7a56d14037f0af
Arturo <artgurid@gmail.com>
2021-07-02 17:44:28 +0200

    New bc connection functions

--- lemlab/bc_connection/bc_connection.py
+++ lemlab/bc_connection/bc_connection.py
@@ -2,6 +2,7 @@ import lemlab.bc_connection.bc_param as bc_param
 import time
 import json
 import os
+import re
 from web3 import Web3, HTTPProvider
 import pandas as pd
 from pathlib import Path
@@ -28,6 +29,7 @@ class BlockchainConnection:
             self.platform = web3_instance.eth.contract(address=contract_address,
                                                        abi=contract_abi,
                                                        bytecode=contract_bytecode)
+            self.web3_eth=web3_instance.eth
             self.coinbase = web3_instance.eth.coinbase
             self.functions = self.platform.functions
         except Exception as e:
@@ -47,11 +49,41 @@ class BlockchainConnection:
                 print("User is not stored.")
         return user_info_df
 
+    def get_list_all_users(self, ts_delivery_active=None, returnList=False):
+        """
+        Returns a list of all the users, either in list format or in dataframe format
+        Parameters
+        ----------
+        ts_delivery_active: optional argument to filter out users by the time they delivered energy
+        returnList: bool: if the returned value is a list of lists or a dataframe
+
+        Returns: Either list of lists or dataframe of all the users registered in the blockchain
+        --------
+
+        """
+        user_info_list = self.functions.get_user_infos().call()
+        user_info_df = pd.DataFrame(user_info_list, columns=bc_param.info_user_column_names)
+        if ts_delivery_active is not None:
+            final_list = []
+            for _, user in user_info_df.iterrows():
+                if user[bc_param.TS_DELIVERY_FIRST] <= ts_delivery_active <= user[bc_param.TS_DELIVERY_LAST]:
+                    final_list.append(dict(user))
+            user_info_list = final_list
+            user_info_df = pd.DataFrame(final_list, columns=bc_param.info_user_column_names)
+        if returnList:
+            return user_info_list
+        else:
+            return user_info_df
+
     # Admins only
 
     def register_user(self, df_user):
         tx_hash = self.functions.push_user_info(tuple(df_user.values.tolist()[0])).transact({'from': self.coinbase})
         return tx_hash
+
+    # TODO: implement these functions ?? Does it makes sense ?
+    # def edit_user(self, df_user):
+    # def delete_user(self, df_user):
 
     ###################################################
     # Functions for the meter registration table
@@ -66,11 +98,91 @@ class BlockchainConnection:
                 print("User is not stored.")
         return meter_info_df
 
+    def get_list_main_meters(self, ts_delivery_active=None, returnList=False):
+        """
+        Function to get the list of the main meters only. It first retrieves all the meters already filtered by time
+        and then uses regex to filter out the main meters, which are the ones with grid in the name
+        Parameters
+        ----------
+        ts_delivery_active: to get only the meters that are at an exact time active
+        returnList: if the return parameter is a list or a dataframe
+
+        Returns
+        -------
+        A list or dataframe of all the main meters
+        """
+
+        all_meters_df = self.get_list_all_meters(ts_delivery_active, returnList=False)
+        search_pattern = re.compile(r'grid')  # we use regex to search for the meters with grid in their type
+        main_meters_list = []
+        for _, meter in all_meters_df.iterrows():
+            if search_pattern.search(str(meter[bc_param.TYPE_METER]).lower()) is not None:
+                main_meters_list.append(dict(meter))
+
+        if returnList:
+            return main_meters_list
+        else:
+            return pd.DataFrame(main_meters_list, columns=bc_param.info_meter_column_names)
+
+    def get_list_all_meters(self, ts_delivery_active=None, returnList=False):
+        """
+        Function to retrieve all the main meters, and filter them out by time if needed
+        Parameters
+        ----------
+        ts_delivery_active: int: if the meter was active at that time, optional parameter to filter out
+        returnList: if the data to be returned is a dataframe (default) or a list
+
+        Returns
+        -------
+
+        """
+        meter_info_list = self.functions.get_id_meters().call()
+        meter_info_df = pd.DataFrame(meter_info_list, columns=bc_param.info_meter_column_names)
+        if ts_delivery_active is not None:
+            final_meter_list = []
+            for _, meter in meter_info_df.iterrows():
+                if meter[bc_param.TS_DELIVERY_FIRST] <= ts_delivery_active <= meter[bc_param.TS_DELIVERY_LAST]:
+                    final_meter_list.append(dict(meter))
+
+            meter_info_list = final_meter_list
+            meter_info_df = pd.DataFrame(final_meter_list, columns=bc_param.info_meter_column_names)
+        if returnList:
+            return meter_info_list
+        else:
+            return meter_info_df
+
     # Admins only
 
     def register_meter(self, df_meter):
         tx_hash = self.functions.push_id_meters(tuple(df_meter.values.tolist()[0])).transact({'from': self.coinbase})
         return tx_hash
+
+    #################################################
+    # Functions for mapping the meters to the users and so
+    # not possible yet, needs the id market agent
+    """def get_mapping_to_user(self):
+        returns a mapping from id_meter and id_market_agent to each user
+    """
+
+    def get_map_to_main_meter(self):
+
+        info_meter = self.get_list_all_meters(returnList=True)
+        info_users = self.get_list_all_users(returnList=True)
+
+        map_grid_meter_to_self = dict([(i, a) for i, a in zip(info_meter["id_meter"], info_meter["id_meter"])])
+
+        map_user_to_meter = dict([(i, a) for i, a in zip(info_meter["id_user"], info_meter["id_meter"])])
+
+        info_user = self._query_data_free(f"SELECT {self.db_param.ID_USER}, {self.db_param.ID_MARKET_AGENT}"
+                                          f" FROM {self.db_param.NAME_TABLE_INFO_USER}")
+
+        map_ma_to_user = dict([(i, a) for i, a in zip(info_user["id_market_agent"], info_user["id_user"])])
+
+        map_everything_to_main_meter = {}
+        for id_ma in map_ma_to_user:
+            map_everything_to_main_meter[id_ma] = map_user_to_meter.get(map_ma_to_user[id_ma], "0000000000")
+        map_everything_to_main_meter = {**map_everything_to_main_meter, **map_grid_meter_to_self, **map_user_to_meter}
+        return map_everything_to_main_meter
 
     ###################################################
     # Functions for the market bid submission table
@@ -102,6 +214,31 @@ class BlockchainConnection:
             return
         return tx_hash
 
+    ###################################################
+    # Functions for the market clearing of data
+    # Market participants only
+    def clearTempData(self):
+        try:
+            tx_hash = self.functions.clearTempData().transact({'from': self.coinbase})
+            self.wait_for_transact(tx_hash)
+        except:
+            # exceptions happens when the cost of deletion is too big. then we have to delete chunk by chunk
+            limit_to_remove = 500
+            while len(getOffers_or_Bids(isOffer=True, temp=True)) > 0 or len(
+                    getOffers_or_Bids(isOffer=False, temp=True)) > 0 or len(
+                functions.getTempMarketResults().call()) > 0 or len(functions.getMarketResultsTotal().call()) > 0:
+                try:
+                    tx_hash = functions.clearTempData_gas_limit(limit_to_remove).transact({'from': coinbase})
+                    web3_instance.eth.waitForTransactionReceipt(tx_hash)
+                except:
+                    limit_to_remove -= 50
+
+    ###################################################
+    # Utility functions
+    # Internal use only
+    def wait_for_transact(self, tx_hash):
+        self.web3_eth.waitForTransactionReceipt(tx_hash)
+
 
 if __name__ == "__main__":
     block_dict = {"host": "localhost",
@@ -112,23 +249,31 @@ if __name__ == "__main__":
 
     bc_lem_conn = BlockchainConnection(block_dict)
 
-    print(bc_lem_conn.get_info_user(user_id="7434RSBU"))
+    print(bc_lem_conn.get_info_user(user_id="9022DLBF"))
 
-    new_user = pd.DataFrame(data=[["TESTUSER", 1000, 0, 10000, 100, 'green', 10, 'zi', 0, 0, 0]],
+    new_user = pd.DataFrame(data=[["7961MZAC", 1000, 0, 10000, 100, 'green', 10, 'zi', 0, "3991MRTC", 0, 0]],
                             columns=bc_param.info_user_column_names)
     bc_lem_conn.register_user(df_user=new_user)
 
-    print(bc_lem_conn.get_info_user(user_id="TESTUSER"))
+    all_users_df = bc_lem_conn.get_list_all_users()
 
-    new_meter_df = pd.DataFrame(data=[["TESTMETER", "TESTUSER", 1, "0", 'aggregator', 'local', 0, 0, 'test']],
-                                columns=bc_param.info_meter_column_names)
+    print(all_users_df.head(n=8))
+
+    print(bc_lem_conn.get_info_user(user_id="7961MZAC"))
+
+    new_meter_df = pd.DataFrame(
+        data=[["6543MZUG", "3214KOPL", "0", "virtual grid meter", 'aggregator', 'green', 0, 0, 'test']],
+        columns=bc_param.info_meter_column_names)
     bc_lem_conn.register_meter(df_meter=new_meter_df)
 
-    print(bc_lem_conn.get_info_meter(meter_id="TESTMETER"))
+    print("Meter:", bc_lem_conn.get_info_meter(meter_id="6543MHUJ"))
 
-    new_position = pd.DataFrame(data=[["TESTUSER", 100, 10, 1, 10, "bid", 0, 0, round(time.time()), 123412]],
-                                columns=bc_param.positions_market_ex_ante_column_names)
+    all_meters_df = bc_lem_conn.get_list_main_meters()
+
+    print("All meters", all_meters_df.head(n=8))
+
+    # new_position = pd.DataFrame(data=[["TESTUSER", 100, 10, 1, 10, "bid", 0, 0, round(time.time()), 123412]],
+    #                            columns=bc_param.positions_market_ex_ante_column_names)
     # bc_lem_conn.push_position(df_position=new_position)
 
     # print(bc_lem_conn.get_open_positions())
-
--- lemlab/bc_connection/bc_param.py
+++ lemlab/bc_connection/bc_param.py
@@ -1,13 +1,106 @@
 from sqlalchemy import Text, BigInteger
 
+# Column names (sorted alphabetically)
+BALANCE_ACCOUNT = 'balance_account'
+DELTA_BALANCE = 'delta_balance'
+ENERGY_BALANCING_NEGATIVE = 'energy_balancing_negative'
+ENERGY_BALANCING_POSITIVE = 'energy_balancing_positive'
+ENERGY_CUMULATED = 'energy_cumulated'
+ENERGY_IN = 'energy_in'
+ENERGY_IN_CUM = 'energy_in_cum'
+ENERGY_OUT = 'energy_out'
+ENERGY_OUT_CUM = 'energy_out_cum'
+EXTENSION_BID = '_bid'
+EXTENSION_OFFER = '_offer'
+HORIZON_TRADING = 'horizon_trading'
+ID_AGGREGATOR = 'id_aggregator'
+ID_BID = 'id_bid'
+ID_MARKET_AGENT = 'id_market_agent'
+ID_MATCHING = 'id_matching'
+ID_METER = 'id_meter'
+ID_METER_SUPER = 'id_meter_super'
+ID_OFFER = 'id_offer'
+ID_SOURCE = 'id_source'
+ID_USER = 'id_user'
+ID_USER_BID = 'id_user_bid'
+ID_USER_OFFER = 'id_user_offer'
+INFO_ADDITIONAL = 'info_additional'
+NUMBER_POSITION = 'number_position'
+NUMBER_POSITION_BID = 'number_position_bid'
+NUMBER_POSITION_OFFER = 'number_position_offer'
+PREFERENCE_QUALITY = 'preference_quality'
+PREMIUM_PREFERENCE_QUALITY = 'premium_preference_quality'
+PRICE_ENERGY = 'price_energy'
+PRICE_ENERGY_BALANCING_NEGATIVE = 'price_energy_balancing_negative'
+PRICE_ENERGY_BALANCING_POSITIVE = 'price_energy_balancing_positive'
+PRICE_ENERGY_BID = 'price_energy_bid'
+PRICE_ENERGY_BID_MAX = 'price_energy_bid_max'
+PRICE_ENERGY_BID_MIN = 'price_energy_bid_min'
+PRICE_ENERGY_LEVIES_NEGATIVE = "price_energy_levies_negative"
+PRICE_ENERGY_LEVIES_POSITIVE = "price_energy_levies_positive"
+PRICE_ENERGY_MARKET = 'price_energy_market'
+PRICE_ENERGY_OFFER = 'price_energy_offer'
+PRICE_ENERGY_OFFER_MAX = 'price_energy_offer_max'
+PRICE_ENERGY_OFFER_MIN = 'price_energy_offer_min'
+QTY_ENERGY = 'qty_energy'
+QTY_ENERGY_BIDS_CUM = "qty_energy_bids_cum"
+QTY_ENERGY_OFFERS_CUM = "qty_energy_offers_cum"
+QTY_ENERGY_TRADED = 'qty_energy_traded'
+QTY_ENERGY_TRADED_CUM = "qty_energy_traded_cum"
+QUALITY_ENERGY = 'quality_energy'
+QUALITY_ENERGY_BID = 'quality_energy_bid'
+QUALITY_ENERGY_MARKET = 'quality_energy_market'
+QUALITY_ENERGY_OFFER = 'quality_energy_offer'
+STATUS_METER_READINGS_PROCESSED = 'status_meter_readings_processed'
+STATUS_POSITION = 'status_position'
+STATUS_SETTLEMENT_COMPLETE = 'status_settlement_complete'
+STRATEGY_MARKET_AGENT = 'strategy_market_agent'
+TS_DELIVERY = 'ts_delivery'
+TS_DELIVERY_FIRST = 'ts_delivery_first'
+TS_DELIVERY_LAST = 'ts_delivery_last'
+TYPE_METER = 'type_meter'
+TYPE_POSITION = 'type_position'
+TYPE_TRANSACTION = 'type_transaction'
+T_CLEARED = 't_cleared'
+T_READING = 't_reading'
+T_SUBMISSION = 't_submission'
+T_UPDATE_BALANCE = 't_update_balance'
 
-info_user_column_names = ["id_user", "balance_account", "t_update_balance", "price_energy_bid_max",
-                          "price_energy_offer_min", "preference_quality", "premium_preference_quality",
-                          "type_market_agent", "horizon_trading", "ts_delivery_first", "ts_delivery_last"]
-info_meter_column_names = ["id_meter", "id_user", "type_meter", "id_meter_main", "id_aggregator", "quality_energy",
-                           "ts_delivery_first", "ts_delivery_last", "info_additional"]
-positions_market_ex_ante_column_names = ["id_user", "qty_energy", "price_energy", "quality_energy",
-                                         "premium_preference_quality", "type_position", "number_position",
-                                         "status_position", "t_submission", "ts_delivery"]
+# Column base names to be dynamically added
+PRICE_ENERGY_MARKET_ = PRICE_ENERGY_MARKET + '_'
+SHARE_QUALITY_ = 'share_quality_offers_cleared_'
+
+SHARE_PREFERENCE_BIDS_ = 'share_preference_bids_'
+SHARE_QUALITY_OFFERS_ = 'share_quality_offers_'
+SHARE_PREFERENCE_BIDS_CLEARED_ = 'share_preference_bids_cleared_'
+SHARE_QUALITY_OFFERS_CLEARED_ = 'share_quality_offers_cleared_'
+
+# Conversion factors
+EURO_TO_SIGMA = 1e9  # conversion rate from euro to internal currency sigma
+
+info_user_column_names = [ID_USER, BALANCE_ACCOUNT, T_UPDATE_BALANCE, PRICE_ENERGY_BID_MAX,
+                          PRICE_ENERGY_OFFER_MIN, PREFERENCE_QUALITY, PREMIUM_PREFERENCE_QUALITY,
+                          STRATEGY_MARKET_AGENT, HORIZON_TRADING, ID_MARKET_AGENT,
+                          TS_DELIVERY_FIRST, TS_DELIVERY_LAST]
+
 info_user_column_dtypes = [Text(), BigInteger(), BigInteger(), BigInteger(), BigInteger(), Text(), BigInteger(),
                            Text(), BigInteger(), BigInteger(), BigInteger()]
+
+info_meter_column_names = [ID_METER, ID_USER, ID_METER_SUPER, TYPE_METER, ID_AGGREGATOR, QUALITY_ENERGY,
+                           TS_DELIVERY_FIRST, TS_DELIVERY_LAST, INFO_ADDITIONAL]
+
+info_meter_column_dtypes = [Text(), Text(), Text(), Text(), Text(), Text(), BigInteger(), BigInteger(), Text()]
+
+positions_market_ex_ante_column_names = [ID_USER, QTY_ENERGY, PRICE_ENERGY, QUALITY_ENERGY, PREMIUM_PREFERENCE_QUALITY,
+                                         TYPE_POSITION, NUMBER_POSITION, STATUS_POSITION, T_SUBMISSION, TS_DELIVERY]
+positions_market_ex_ante_column_dtypes = [Text(), BigInteger(), BigInteger(), Text(), BigInteger(), Text(),
+                                          BigInteger(), BigInteger, BigInteger(), BigInteger()]
+
+
+def map_name_to_dtype(names_column, dtypes_column):
+    mapping = {}
+    assert len(names_column) == len(dtypes_column)
+    for name, dtype in zip(names_column, dtypes_column):
+        mapping[name] = dtype
+
+    return mapping
